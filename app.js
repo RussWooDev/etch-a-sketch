7ff83710dcd5f26da8eef415da831bb44b558299
russwoodev <russwoodev9@gmail.com>
2023-01-11 23:32:19 +0800

    Finished foundation

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
-const addSquares = (size) => {
-    let board = document.querySelector('.sketchboard');
-    board.style.gridTemplateColumns = 'repeat(16 , 1fr)'
-    board.style.gridTemplateRows = 'repeat(16 , 1fr)'
-   
-    for(let i=0; i<256; i++){
-        let square = document.createElement('div');
-        square.style.backgroundColor="gray";
-        board.insertAdjacentElement('beforeend' , square)
-    }
-} 
 
-addSquares(16)
+const addSquares = (size) => {
+    let board = document.querySelector(".board");
+    let squares = board.querySelectorAll("div");
+    squares.forEach((div) => div.remove());
+    board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    board.style.gridTemplateRows =`repeat(${size}, 1fr)`;
+
+
+    for(i=0; i<(size*size); i++){
+        let square = document.createElement('div');
+        square.style.backgroundColor = "white";
+        square.style.borderRight = "thin solid darkgrey";
+        square.style.borderBottom = "thin solid darkgrey";
+
+        square.addEventListener("mousedown", () => square.style.backgroundColor = "black");
+        board.insertAdjacentElement('beforeend', square); 
+    }
+}
+
+addSquares(16);
+
+
+const changeSize = (input) => {
+    if(input < 2) return console.log("too few squares");
+    if(input > 100) return console.log("too many squares");
+    addSquares(input);
+}
